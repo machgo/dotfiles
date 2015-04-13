a424b63667bcb04b22b984af64b9b9ca301e639a
Marco Schmid <fakE00r@gmail.com>
2015-04-13 19:44:25 +0200

    removed slow printscreen-lock

--- i3lock.sh
+++ i3lock.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #copied from https://github.com/tunnelshade/
 
-scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 2x2 /tmp/screen_locked2.png
-i3lock -i /tmp/screen_locked2.png
+i3lock 
