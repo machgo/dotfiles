75971ab76ce30f19d124ed4151ecd299ad97222e
Marco Schmid <marco.schmid@eda.admin.ch>
2022-09-29 08:22:38 +0200

    added lua and vimls lsp

--- nvim/nvim-cmp.lua
+++ nvim/nvim-cmp.lua
@@ -63,6 +63,9 @@
   -- Set up lspconfig.
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+  require('lspconfig')['sumneko_lua'].setup {
+    capabilities = capabilities
+  }
+  require('lspconfig')['vimls'].setup {
     capabilities = capabilities
   }
