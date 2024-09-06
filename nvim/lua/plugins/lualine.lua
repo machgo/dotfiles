cda7721562c2b50bdaa14ceda001a5002d5be250
Marco Schmid <marco.schmid@eda.admin.ch>
2024-09-06 12:36:55 +0200

    added some settings

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,4 +1,21 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        icons_enabled = true,
+        theme = 'auto'
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = {
+            {
+                'buffers',
+                mode = 0
+            }
+        },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
 }
