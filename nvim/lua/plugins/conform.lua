e2eda8f767fdbc1a786ffd81e4becc63ff13d9df
Marco Schmid <marco.schmid@eda.admin.ch>
2024-09-25 16:01:08 +0200

    added yaml formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,26 +1,27 @@
 return {
-	"stevearc/conform.nvim",
-	cmd = { "ConformInfo" },
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			-- Conform will run multiple formatters sequentially
-			python = { "isort", "black" },
-			-- You can customize some of the format options for the filetype (:help conform.format)
-			rust = { "rustfmt", lsp_format = "fallback" },
-			-- Conform will run the first available formatter
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-		},
-		format_on_save = { timeout_ms = 500 },
-	},
-	keys = {
-		{
-			"<leader>cc",
-			function()
-				require("conform").format({ async = true })
-			end,
-			mode = "",
-			desc = "Format buffer",
-		},
-	},
+    "stevearc/conform.nvim",
+    cmd = { "ConformInfo" },
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            -- Conform will run multiple formatters sequentially
+            python = { "isort", "black" },
+            -- You can customize some of the format options for the filetype (:help conform.format)
+            rust = { "rustfmt", lsp_format = "fallback" },
+            -- Conform will run the first available formatter
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+            yaml = { "yamlfmt" }
+        },
+        format_on_save = { timeout_ms = 500 },
+    },
+    keys = {
+        {
+            "<leader>cc",
+            function()
+                require("conform").format({ async = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
 }
