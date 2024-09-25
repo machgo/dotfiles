4e0d32fa335dd18b2b988bbb1c6feef90d7077c3
Marco Schmid <marco.schmid@eda.admin.ch>
2024-09-25 09:53:18 +0200

    added options

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,5 +1,11 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
-    opts = {},
+    opts = {
+        file_types = { 'markdown' },
+        code = {
+            width = 'block',
+            border = 'thick',
+        },
+    },
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
 }
