2800e081a09af8947ee239c20e9fb1f1c631bb10
Marco Schmid <marco.schmid@eda.admin.ch>
2024-11-15 10:53:55 +0100

    added telescope undo

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,10 +4,26 @@ return {
     event = "VimEnter",
     dependencies = {
         "nvim-lua/plenary.nvim",
+        "debugloop/telescope-undo.nvim",
     },
     keys = {
         { "<leader>bb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>" },
         { "<leader>ff", "<cmd>Telescope find_files<cr>" },
         { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+        { "<leader>fu", "<cmd>Telescope undo<cr>" },
     },
+    config = function()
+        require("telescope").setup({
+            -- the rest of your telescope config goes here
+            extensions = {
+                undo = {
+                    -- telescope-undo.nvim config, see below
+                },
+                -- other extensions:
+                -- file_browser = { ... }
+            },
+        })
+        require("telescope").load_extension("undo")
+        -- optional: vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
+    end,
 }
