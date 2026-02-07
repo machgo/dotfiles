6d501b26742e33eed79b5932657888ec01184785
Marco Schmid <marco@Marcos-MacBook-Air.local>
2026-02-07 16:01:48 +0100

    fixed autoformatting

--- nvim/lua/marco/plugins/conform.lua
+++ nvim/lua/marco/plugins/conform.lua
@@ -1,6 +1,10 @@
 return {
 	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
+	-- This will provide type hinting with LuaLS
+	---@module "conform"
+	---@type conform.setupOpts
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
@@ -21,6 +25,10 @@ return {
 			ps1 = { lsp_format = "prefer" },
 			hclpacker = { "packer_fmt" },
 		},
+
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
 		format_on_save = { timeout_ms = 500 },
 	},
 	keys = {
@@ -33,4 +41,8 @@ return {
 			desc = "Format buffer",
 		},
 	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }
