341ec36209bf6d05291babb70db67e524ad8df55
Marco Schmid <marco@Marcos-MacBook-Air.local>
2026-02-07 16:31:49 +0100

    fixed lualine to display lsp

--- nvim/lua/marco/plugins/lualine.lua
+++ nvim/lua/marco/plugins/lualine.lua
@@ -1,21 +1,23 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-        icons_enabled = true,
-        theme = 'auto'
-    },
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = {
-            {
-                'buffers',
-                mode = 0
-            }
-        },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-    },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "auto",
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = {
+				{
+					"buffers",
+					mode = 0,
+				},
+			},
+			lualine_x = { "encoding", "fileformat", "filetype", "lsp_status" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+	},
 }
