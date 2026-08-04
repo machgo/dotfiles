3b72d22ea1fea323eddc8ec8043fbefb9bb9edbf
Marco Schmid <marco@Marcos-MacBook-Air.local>
2026-08-04 21:31:16 +0200

    added mason tool installer

--- nvim/lua/marco/plugins/mason.lua
+++ nvim/lua/marco/plugins/mason.lua
@@ -1,12 +1,43 @@
 return {
-    "mason-org/mason.nvim",
-     opts = {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            }
-        }
-    }
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "mason-org/mason.nvim" },
+		opts = {
+			ensure_installed = {
+				-- LSP servers
+				"ansible-language-server",
+				"gopls",
+				"lua-language-server",
+				"marksman",
+				"powershell-editor-services",
+				"pyright",
+				"terraform-ls",
+				"yaml-language-server",
+
+				-- Linters and formatters
+				"ansible-lint",
+				"black",
+				"hclfmt",
+				"isort",
+				"prettier",
+				"prettierd",
+				"stylua",
+				"terraform",
+			},
+			run_on_start = true,
+			auto_update = true,
+		},
+	},
 }
